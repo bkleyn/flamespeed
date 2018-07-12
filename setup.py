f2df8e7be73cfbedd20ead8ebd1c6d8d6e76a9f6
bkleyn <bernardkleynhans@g.harvard.edu>
2018-07-12 10:30:41 -0400

    Update link to repo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email = 'student@g.harvard.edu',
     packages=find_packages(),
     package_data={'flamespeed': ['./data/*.*', './data/rxns_test/*.*']},
-    url='https://github.com/flamespeed/cs207-FinalProject/',
+    url='https://github.com/bkleyn/flamespeed.git/',
     license='MIT',
     description='Chemical kinetics library for Python',
     long_description=open('README.md').read(),
